ad15b44bacde749fb47370e03974e519b38e7e40
byulla <areyh817@gmail.com>
2026-07-23 17:45:09 +0900

    라이다-앱 실전 관통: LIMO 서버(무설치판) + 앱 로봇 연결

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'package:vibration/vibration.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 // ★ 서버 주소: 시뮬레이터=localhost, 실기기=맥/로봇 IP
-const String serverUrl = 'ws://localhost:8765';
+const String serverUrl = 'ws://192.168.55.1:8765';
 
 // ===== 색상 (목업 팔레트) =====
 const cGreenBg = Color(0xFFEAF3DE);
